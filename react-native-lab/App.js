3d7cf8b05a59c31c94582a5b672959c1f9c82f14
mateonoel2 <mateo.noel@utec.edu.pe>
2023-11-09 15:26:23 -0500

    con cloudinary

--- react-native-lab/App.js
+++ react-native-lab/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { useEffect, useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
-  const [data, setData] = useState();
+  const [data, setData] = useState('');
   const [facts, setFacts] = useState({});
   const [number, setNumber] = useState(0);
+  const [image, setImage] = useState(null);
 
   useEffect(() => {
     fetch('https://cat-fact.herokuapp.com/facts')
@@ -14,7 +16,15 @@ export default function App() {
         setFacts(json);
         setData(json[0].text);
       })
-      .catch((error) => console.error(error))
+      .catch((error) => console.error(error));
+
+    // Solicitar permisos para acceder a la cámara
+    (async () => {
+      const { status } = await ImagePicker.requestCameraPermissionsAsync();
+      if (status !== 'granted') {
+        alert('Permiso denegado para acceder a la cámara');
+      }
+    })();
   }, []);
 
   const onPressLearnMore = () => {
@@ -27,16 +37,59 @@ export default function App() {
     }
   }
 
+  const openCamera = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    
+    if (!result.canceled) {
+      // Subir la imagen a Cloudinary o cualquier otro servicio en la nube
+      const formData = new FormData();
+      formData.append('file', {
+        uri: result.assets[0].uri,
+        type: 'image/jpeg',
+        name: 'photo.jpg',
+      });
+    
+      const cloudinaryCloudName = 'dalsog2hb';
+      const cloudinaryUploadPreset = 'alphtdg5';
+      const cloudinaryApiKey = '778342524695887';
+
+      const cloudinaryUploadUrl = `https://api.cloudinary.com/v1_1/${cloudinaryCloudName}/image/upload?upload_preset=${cloudinaryUploadPreset}&api_key=${cloudinaryApiKey}`;
+
+      fetch(cloudinaryUploadUrl, {
+        method: 'POST',
+        body: formData,
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+        .then(response => response.json())
+        .then(responseData => {
+          console.log('Imagen subida con éxito a Cloudinary:', responseData);
+        })
+        .catch(error => {
+          console.error('Error al subir la imagen a Cloudinary:', error);
+        });
+
+      setImage(result.assets[0].uri);
+    }
+  };
+
+
+
   return (
     <View style={styles.container}>
       <Text style={{ fontWeight: 'bold' }}>Cat Facts</Text>
-      <View style={{ margin: 20 }}></View>
+      <View style={{ margin: 20 }}>
+        {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      </View>
       <Text>{data}</Text>
-      <Button
-        onPress={onPressLearnMore}
-        title="Change fact"
-        color="blue"
-      />
+      <Button onPress={onPressLearnMore} title="Change fact" color="blue" />
+      <Button onPress={openCamera} title="Open Camera" color="green" />
       <StatusBar style="auto" />
     </View>
   );
